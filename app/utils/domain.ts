c7e742ddbca9240decf0f7a7386855370a5fb5da
Claude Stark <claude.m.stark@gmail.com>
2026-04-29 10:16:55 +0200

    fix: debug ApiDomain

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -6,6 +6,8 @@ import { isProduction } from "@/utils/environment";
  * In development, it returns "http://localhost:8080".
  */
 export function getApiDomain(): string {
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("PROD URL:", process.env.NEXT_PUBLIC_PROD_API_URL);
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
     "https://api.guessbb.claudestark.ch"; // TODO: update with your production URL as needed.
   const devUrl = "http://localhost:8080";
